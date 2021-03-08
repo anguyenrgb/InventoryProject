cf0fc0d18b46dd43a84d1a95ba30a4b4a8d9908a
anguyenrgb <80228077+anguyenrgb@users.noreply.github.com>
2021-03-07 16:52:43 -0800

    Create dbhandler.php

---
+++ inc/dbhandler.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "inventory_system"; 
+
+$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbname);
