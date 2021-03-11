9184e68840183d2436fee4a93dfb6b21d65b283d
anguyenrgb <80228077+anguyenrgb@users.noreply.github.com>
2021-03-11 12:06:12 -0800

    Update PCCheckin.php

--- inc/PCCheckin.php
+++ inc/PCCheckin.php
@@ -6,8 +6,10 @@
 	$Model = ($_POST['ItemNotes']);
 	$Cart = ($_POST['Cart']);
 	$DateOrdered = ($_POST['Date']);
+	$PartType = ($_POST['PartType']);
+	$Issue = ($_POST['Issue']);
 
-	$sql = "INSERT INTO rack_1 (ServiceOrder, SKU, Model, Cart, DateOrdered) VALUES ('$SO', '$SKU', '$Model', '$Cart', '$DateOrdered');";
+	$sql = "INSERT INTO rack_1 (ServiceOrder, SKU, Model, Cart, DateOrdered, PartType, Issue) VALUES ('$SO', '$SKU', '$Model', '$Cart', '$DateOrdered', '$PartType', '$Issue');";
 	mysqli_query($conn, $sql);
 
 	header("Location: http://localhost/CheckIn.php");
